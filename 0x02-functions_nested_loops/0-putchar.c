de0a3de8861ff801f559acfa1ae3b17e5ff91615
riverflo88 <riverflo88@yahoo.com>
2023-09-14 07:32:14 -0700

    added main.h file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <main.h>
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -7,7 +8,14 @@
  */
 int main(void)
 {
-	char letter[] = "_putchar";
-	printf("%s",letter);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n')
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Header file for all prototypes
+ * used in 0x02-functions_nested_loops
+ */
+int _putchar(char c)
+
+#endif
