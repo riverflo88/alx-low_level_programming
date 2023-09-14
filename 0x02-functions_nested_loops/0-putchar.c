0ade787b06f0f18ab0ac713213b1c1fd63368773
riverflo88 <riverflo88@yahoo.com>
2023-09-14 06:29:53 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Simply prints _putchar followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char letter[] = "_putchar";
+	printf("%s",letter);
+	return (0);
+}
