e7d824d23cb27def636694cfbcbcd3339b6167aa
riverflo88 <riverflo88@yahoo.com>
2023-09-15 09:09:49 -0700

    update 9-times_table x13

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,15 @@ void times_table(void)
         for(j = 0; j < 10; j++)
         {
             k = i * j;
-            if(j == 0)
+            if(k < 10)
+            {
                 _putchar(' ');
-            
-            if ((k % 10) > 0)
-                _putchar((k % 10) + '0');
+                _putchar(k + '0');
+            }
             else
-                _putchar('0');
-                
+                _putchar((k / 10) + '0');
+                _putchar((k % 10) + '0');
+
             if (j < 9)
             {
                 _putchar(',');
