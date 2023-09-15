997c9aa880cd82cf4d76495a1f0f3283284d334a
riverflo88 <riverflo88@yahoo.com>
2023-09-15 02:10:46 -0700

    updated  3-lower 3X

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,14 +6,15 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * _islower -returns 0 if the input is lowercase or 1 otherwise
+ * _islower - checks if an input is lowercase
+ * @c: the input to be checked
+ *
+ * return: 1 if the input is lowercase or 0 otherwise
 */
 int _islower(int c)
 {
-    if (islower(c))
-    {
+    if (islower(c)
         return 1;
-    }else{
+    else
         return 0;
-    }
 }
