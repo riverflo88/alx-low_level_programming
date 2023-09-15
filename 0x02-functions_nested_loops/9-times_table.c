90a6e8397ec180ee34301e924038d2fede6e544c
riverflo88 <riverflo88@yahoo.com>
2023-09-15 06:26:38 -0700

    add 9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+/**
+ * Author: Dr. Okoye C.S
+ * File: 9-times_table.c
+*/
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - prints the 9 times table 
+*/
+void times_table(void)
+{
+    int i,j,k;
+    i = 0;
+    j = 0;
+    while (i < 10)
+    {
+        while (j<10)
+        {
+            k = i * j;
+            
+            putchar((k / 10) + 0);
+            if ((k % 10) > 0)
+                putchar((k % 10) + 0);
+
+            j++;
+            _putchar(',');
+            _putchar(' ');
+        }
+
+        _putchar('$');
+        i++;
+    }
+}
+
