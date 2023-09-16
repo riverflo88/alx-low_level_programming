44937d1dab3ccec57f7a01e24a6ca55c81ed2b46
riverflo88 <riverflo88@yahoo.com>
2023-09-16 16:50:21 -0700

    add 100-times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,7 +28,7 @@ void print_times_table(int n)
 	        		_putchar(' ');
 				_putchar(' ');
                 		_putchar(' ');
-                		_putchar(k + '0')
+                		_putchar(k + '0');
 	    
             		}
 	    		else if( k >= 100 )
@@ -40,7 +40,7 @@ void print_times_table(int n)
 	    		}
             		else 
 	    		{
-    	     			_putchar(' ')
+    	     			_putchar(' ');
                 		_putchar(' ');
 	        		_putchar((k / 10) + '0');
                 		_putchar((k % 10) + '0');
