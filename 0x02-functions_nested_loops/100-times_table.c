71e59fc656c547337177f53e204393a48c0e81cb
riverflo88 <riverflo88@yahoo.com>
2023-09-16 16:46:56 -0700

    add 100-times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,45 +11,46 @@
 */
 void print_times_table(int n)
 {
-    int i,j,k;
+	int i,j,k;
 
-    for(i = 0; i < n; i++)
-    {	if (n > 15 || n < 0)
-	    break
-        for(j = 0; j < n; j++)
-        {
-            k = i * j;
-            if (j == 0)
-                _putchar(k + '0');
+	for(i = 0; i < n; i++)
+	{	
+		if (n > 15 || n < 0)
+			break;
+        	for(j = 0; j < n; j++)
+        	{
+            		k = i * j;
+            		if (j == 0)
+                		_putchar(k + '0');
             
-            else if(k < 10 )
-            {
-	        _putchar(' ');
-		_putchar(' ');
-                _putchar(' ');
-                _putchar(k + '0')
+            		else if(k < 10 )
+            		{
+	        		_putchar(' ');
+				_putchar(' ');
+                		_putchar(' ');
+                		_putchar(k + '0')
 	    
-            }
-	    else if( k >= 100 )
-            {
-                _putchar(' ');
-                _putchar((k / 100) + '0'); 
-		_putchar((k /10 ) % 10 + '0');
-		_putchar(((k % 100)) % 10 + '0');
-	    }
-            else 
-	    {
-    	     	_putchar(' ')
-                _putchar(' ');
-	        _putchar((k / 10) + '0');
-                _putchar((k % 10) + '0');
-	    }
-            if (j < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+            		}
+	    		else if( k >= 100 )
+            		{
+                		_putchar(' ');
+                		_putchar((k / 100) + '0'); 
+				_putchar((k /10 ) % 10 + '0');
+				_putchar(((k % 100)) % 10 + '0');
+	    		}
+            		else 
+	    		{
+    	     			_putchar(' ')
+                		_putchar(' ');
+	        		_putchar((k / 10) + '0');
+                		_putchar((k % 10) + '0');
+	    		}
+            		if (j < 9)
+            		{
+                		_putchar(',');
+                		_putchar(' ');
+            		}
+        	
+		_putchar('\n');
+	}
 }
