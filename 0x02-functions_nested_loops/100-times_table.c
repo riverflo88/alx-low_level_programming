3ecf6d937e8522d6cd5bbd0256608ca92ddeaf21
riverflo88 <riverflo88@yahoo.com>
2023-09-16 17:05:21 -0700

    add 100-times tableX3

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,11 +13,11 @@ void print_times_table(int n)
 {
 	int i,j,k;
 
-	for(i = 0; i < n; i++)
+	for(i = 0; i <= n; i++)
 	{	
 		if (n > 15 || n < 0)
 			break;
-        	for(j = 0; j < n; j++)
+        	for(j = 0; j <= n; j++)
         	{
             		k = i * j;
             		if (j == 0)
