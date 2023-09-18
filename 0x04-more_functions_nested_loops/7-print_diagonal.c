f3bc2955717bb02ce6f839af2053f25d97c414ed
riverflo88 <riverflo88@yahoo.com>
2023-09-18 07:43:16 -0700

    update 7-diagonal x1

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,9 +8,7 @@
 void print_diagonal(int n)
 {
 	if (n <= 0)
-	{
-		_putchar('\n');
-	}
+        {}
 	else
 	{
 		int i, j;
@@ -31,4 +29,5 @@ void print_diagonal(int n)
 			_putchar('\n');
 		}
 	}
+    _putchar('\n');
 }
