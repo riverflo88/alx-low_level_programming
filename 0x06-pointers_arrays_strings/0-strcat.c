dbbf70716b9968dfb7679915e1d61a4a325d7f4d
riverflo88 <riverflo88@yahoo.com>
2023-09-23 11:08:18 -0700

    adding 0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+char *_strcat(char *dest, char *src)
+{
+
+    int i = 0;
+    int j = 0;
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0'; 
+    return (dest);
+        
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+char *_strcat(char *dest, char *src);
+
+
+#endif
