6e2a98628ce2f1014cab434dbeecd1f30d25b380
riverflo88 <riverflo88@yahoo.com>
2023-10-20 00:52:56 -0700

    0-print

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -11,17 +11,15 @@ void print_name(char *name, void (*f)(char *))
 {
 	char *name_copy;
 
-	if (!name || !f)/*if name and function is NULL*/
+	if (!name && !f)
 		return;
-	/*allocate memory for a new string name_copy, copy of name*/
-	/*malloc, fun is passed in as argument to allocate enough*/
-	/*memory to hold the name Pul 1 additional byte for null char*/
+	
 	name_copy = malloc(sizeof(char) * (strlen(name) + 1));
 
 	if (!name_copy)
-		return;/*if pointer is null*/
+		return;
 
-	strcpy(name_copy, name);/*copy name string to a new name_copy using strcpy()*/
-	f(name_copy);/*call function pointed to by f and pass in the name_copy*/
-	free(name_copy);/*free memory allocated for name_copy*/
+	strcpy(name_copy, name);
+	f(name_copy);
+	free(name_copy);
 }
