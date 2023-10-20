ce37ee134d378aa233b5575d9a8e88a7ff9f7fbf
riverflo88 <riverflo88@yahoo.com>
2023-10-20 05:09:38 -0700

    assig 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,12 @@
 #include <stddef.h>
 #include "lists.h"
 
+/**
+ *print_list - Function that prints the number of nodes given
+ *@list_t: node/slinked list
+ *
+ *Return: count of the node
+ */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
