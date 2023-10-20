bc01bb1e978193b746f209a42b3d94b4dfa97a2e
riverflo88 <riverflo88@yahoo.com>
2023-10-20 10:31:33 -0700

    assi 4 _1

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,9 +6,9 @@ void free_list(list_t *head)
     while (head != NULL)
     {
         list_t *free_now = head;
-        head = head->next;  // Move to the next node before freeing the current one
-        free(free_now->str); // Free the string
-        free(free_now);      // Free the node
+        head = head->next; 
+        free(free_now->str); 
+        free(free_now);   
     }
 }
 
