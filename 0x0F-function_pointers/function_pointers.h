c5fc5bf65daaa680d047af67853b5fb69f42073e
riverflo88 <riverflo88@yahoo.com>
2023-10-27 06:41:46 -0700

    finish up

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,6 +5,10 @@ void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+
+
 
 
 #endif
